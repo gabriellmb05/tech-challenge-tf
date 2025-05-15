2d18876213fb9453c549558d0aea45f8e2f7a974
Samuel Rosa dos Santos <samuelrosacv@gmail.com>
2025-05-15 09:43:42 -0300

    Add TFLint

--- src/main.tf
+++ src/main.tf
@@ -1,12 +1,13 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.79"
 
   name = var.aws_vpc_name
   cidr = var.aws_vpc_cidr
 
-  azs = var.aws_vpc_azrs
+  azs             = var.aws_vpc_azrs
   private_subnets = var.aws_vpc_private_subnets
-  public_subnets = var.aws_vpc_public_subnets
+  public_subnets  = var.aws_vpc_public_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -15,22 +16,24 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = ">= 5.95"
 
-  cluster_name = var.aws_eks_name
+
+  cluster_name    = var.aws_eks_name
   cluster_version = var.aws_eks_version
 
   enable_cluster_creator_admin_permissions = true
 
   subnet_ids = module.vpc.public_subnets
-  vpc_id = module.vpc.default_vpc_id
+  vpc_id     = module.vpc.default_vpc_id
 
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      min_size = 2
-      max_size = 2
+      min_size     = 2
+      max_size     = 2
       desired_size = 2
 
       instance_types = var.aws_eks_eks_managed_node_groups_instance_types
--- src/providers.tf
+++ src/providers.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2"
     }
   }
 }
